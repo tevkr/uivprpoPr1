133be2011e91c6a696a3e996873966ffd0f6d1c0
nom <vlad.nomerov@yandex.ru>
2024-04-16 18:50:22 +0300

    Insertion sort added.

--- src/main/java/ru/mirea/SortingAlgorithms.java
+++ src/main/java/ru/mirea/SortingAlgorithms.java
@@ -4,15 +4,32 @@ public class SortingAlgorithms {
     private SortingAlgorithms() {
         throw new IllegalStateException("Utility class");
     }
+
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n-1; i++)
-            for (int j = 0; j < n-i-1; j++)
-                if (arr[j] > arr[j+1]) {
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
                     // swap temp and arr[i]
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
     }
 }
